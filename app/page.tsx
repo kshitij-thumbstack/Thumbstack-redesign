024ef37fa63f570a79f08bbe25f25516cca26d78
KSHITIJ-0224 <chavankshitij170@gmail.com>
2026-04-24 16:49:12 +0530

    Added Squilio Project

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/home/Hero";
 import FieldsOfPlay from "./components/home/FieldsOfPlay";
 import PartnerBrands from "./components/home/PartnerBrands";
 import Principles from "./components/home/Principles";
+import SqulioProject from "./components/home/SqulioProject";
 import WorkItem from "./components/home/WorkItem";
 import FromTheStack from "./components/home/FromTheStack";
 import LetsTalk from "./components/home/LetsTalk";
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <div className="h-[100svh] overflow-y-auto w-full snap-y snap-mandatory scroll-smooth relative">
       <Hero />
+      <SqulioProject />
       <FieldsOfPlay />
       <PartnerBrands />
       <Principles />
